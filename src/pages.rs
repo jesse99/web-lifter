c319efed33b56ee53d8b852e758c3ef4c590ae6d
Jesse Vorisek <jesse.vorisek@gmail.com>
2024-03-16 16:28:04 -0700

    Renamed file

--- src/pages.rs
+++ src/pages.rs
@@ -1,11 +1,11 @@
+mod app_state;
 mod errors;
 mod exercise_page;
 mod program_page;
-mod state;
 mod workout_page;
 
+pub use app_state::*;
 pub use errors::*;
 pub use exercise_page::*;
 pub use program_page::*;
-pub use state::*;
 pub use workout_page::*;
