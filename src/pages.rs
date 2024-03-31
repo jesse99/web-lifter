2717e0f6c4883f79595f1ae051c8506daf8cbeb9
Jesse Vorisek <jesse.vorisek@gmail.com>
2024-03-31 13:32:31 -0700

    Moved posts into their own file

--- src/pages.rs
+++ src/pages.rs
@@ -1,11 +1,13 @@
 mod app_state;
 mod errors;
 mod exercise_page;
+mod exercise_post;
 mod program_page;
 mod workout_page;
 
 pub use app_state::*;
 pub use errors::*;
 pub use exercise_page::*;
+pub use exercise_post::*;
 pub use program_page::*;
 pub use workout_page::*;
--- src/pages/exercise_page.rs
+++ src/pages/exercise_page.rs
@@ -6,10 +6,8 @@ use crate::{
     program::Program,
     weights::{self, Weights},
     workout::Workout,
-    VarRepsOptions,
 };
 use anyhow::Context;
-use axum::http::Uri;
 use chrono::Local;
 use serde::{Deserialize, Serialize};
 
@@ -60,52 +58,6 @@ pub fn get_exercise_page(
         .context("failed to render template")?)
 }
 
-pub fn post_next_exercise_page(
-    mut state: SharedState,
-    workout_name: &str,
-    exercise_name: &str,
-    options: Option<VarRepsOptions>,
-) -> Result<Uri, InternalError> {
-    let finished = {
-        let program = &state.read().unwrap().user.program;
-        let workout = program
-            .find(&workout_name)
-            .context("failed to find workout")?;
-        let exercise = workout
-            .find(&ExerciseName(exercise_name.to_owned()))
-            .context("failed to find exercise")?;
-        match exercise {
-            Exercise::Durations(d, _) => d.finished,
-            Exercise::FixedReps(d, _) => d.finished,
-            Exercise::VariableReps(d, _) => d.finished,
-            Exercise::VariableSets(d, _) => d.finished,
-        }
-    };
-
-    if finished {
-        let user = &mut state.write().unwrap().user;
-        if let Err(e) = crate::persist::save(user) {
-            user.errors.push(format!("{e}"));
-        }
-    };
-
-    if finished {
-        complete_set(&mut state, workout_name, exercise_name, options);
-
-        let path = format!("/workout/{workout_name}");
-        let uri = url_escape::encode_path(&path);
-        let uri = uri.parse()?;
-        Ok(uri)
-    } else {
-        advance_set(&mut state, workout_name, exercise_name, options);
-
-        let path = format!("/exercise/{workout_name}/{exercise_name}");
-        let uri = url_escape::encode_path(&path);
-        let uri = uri.parse()?;
-        Ok(uri)
-    }
-}
-
 fn reset_old(state: &SharedState, workout_name: &str, exercise_name: &str) -> bool {
     let program = &mut state.write().unwrap().user.program;
     let workout = program.find_mut(&workout_name).unwrap();
@@ -124,225 +76,6 @@ fn reset_old(state: &SharedState, workout_name: &str, exercise_name: &str) -> bo
     } else {
         exercise.reset(Some(now));
         true
-    }
-}
-
-fn advance_set(
-    state: &mut SharedState,
-    workout_name: &str,
-    exercise_name: &str,
-    options: Option<VarRepsOptions>,
-) {
-    fn in_workset(state: &mut SharedState, workout_name: &str, exercise_name: &str) -> bool {
-        let program = &state.read().unwrap().user.program;
-        let workout = program.find(&workout_name).unwrap();
-        let exercise = workout
-            .find(&ExerciseName(exercise_name.to_owned()))
-            .unwrap();
-        match exercise {
-            Exercise::Durations(_, _) => true,
-            Exercise::FixedReps(d, _) => match d.current_index {
-                SetIndex::Workset(_) => true,
-                _ => false,
-            },
-            Exercise::VariableReps(d, _) => match d.current_index {
-                SetIndex::Workset(_) => true,
-                _ => false,
-            },
-            Exercise::VariableSets(_, _) => true,
-        }
-    }
-
-    fn advance_current(state: &mut SharedState, workout_name: &str, exercise_name: &str) {
-        let name = ExerciseName(exercise_name.to_owned());
-        let var_sets_done: i32 = {
-            let history = &state.read().unwrap().user.history;
-            get_var_reps_done(history, &name).iter().sum() // ok to call this if not var sets
-        };
-
-        let program = &mut state.write().unwrap().user.program;
-        let workout = program.find_mut(&workout_name).unwrap();
-        let exercise = workout
-            .find_mut(&ExerciseName(exercise_name.to_owned()))
-            .unwrap();
-        match exercise {
-            Exercise::Durations(d, e) => match d.current_index {
-                SetIndex::Workset(i) => {
-                    if i + 1 == e.num_sets() {
-                        d.finished = true
-                    } else {
-                        d.current_index = SetIndex::Workset(i + 1);
-                    }
-                }
-                _ => panic!("Expected workset"),
-            },
-            Exercise::FixedReps(d, e) => match d.current_index {
-                SetIndex::Warmup(i) => {
-                    if i + 1 == e.num_warmups() {
-                        d.current_index = SetIndex::Workset(0);
-                    } else {
-                        d.current_index = SetIndex::Warmup(i + 1);
-                    }
-                }
-                SetIndex::Workset(i) => {
-                    if i + 1 == e.num_worksets() {
-                        d.finished = true
-                    } else {
-                        d.current_index = SetIndex::Workset(i + 1);
-                    }
-                }
-            },
-            Exercise::VariableReps(d, e) => match d.current_index {
-                SetIndex::Warmup(i) => {
-                    if i + 1 == e.num_warmups() {
-                        d.current_index = SetIndex::Workset(0);
-                    } else {
-                        d.current_index = SetIndex::Warmup(i + 1);
-                    }
-                }
-                SetIndex::Workset(i) => {
-                    if i + 1 == e.num_worksets() {
-                        d.finished = true
-                    } else {
-                        d.current_index = SetIndex::Workset(i + 1);
-                    }
-                }
-            },
-            Exercise::VariableSets(d, e) => match d.current_index {
-                SetIndex::Workset(i) => {
-                    if var_sets_done >= e.target() {
-                        d.finished = true;
-                    } else {
-                        d.current_index = SetIndex::Workset(i + 1);
-                    }
-                }
-                _ => panic!("Expected workset"),
-            },
-        }
-    }
-
-    fn append_result(
-        state: &mut SharedState,
-        workout_name: &str,
-        exercise_name: &str,
-        options: Option<VarRepsOptions>,
-    ) {
-        let name = ExerciseName(exercise_name.to_owned());
-        let (duration, reps, weight) = {
-            let weights = &state.read().unwrap().user.weights;
-            let program = &state.read().unwrap().user.program;
-            let workout = program.find(&workout_name).unwrap();
-            let exercise = workout.find(&name).unwrap();
-            match exercise {
-                Exercise::Durations(d, e) => (
-                    Some(e.set(d.current_index)),
-                    None,
-                    exercise.closest_weight(weights, d.current_index),
-                ),
-                Exercise::FixedReps(d, e) => (
-                    None,
-                    Some(e.set(d.current_index).reps),
-                    match d.current_index {
-                        SetIndex::Warmup(_) => exercise.closest_weight(weights, d.current_index),
-                        SetIndex::Workset(_) => exercise.lower_weight(weights, d.current_index),
-                    },
-                ),
-                Exercise::VariableReps(d, _) => (
-                    None,
-                    options.map(|o| o.reps),
-                    match d.current_index {
-                        SetIndex::Warmup(_) => exercise.closest_weight(weights, d.current_index),
-                        SetIndex::Workset(_) => exercise.lower_weight(weights, d.current_index),
-                    },
-                ),
-                Exercise::VariableSets(d, _) => (
-                    None,
-                    options.map(|o| o.reps),
-                    exercise.lower_weight(weights, d.current_index),
-                ),
-            }
-        };
-        if let Some(duration) = duration {
-            let history = &mut state.write().unwrap().user.history;
-            history.append_duration(&name, duration, weight.map(|w| w.value()));
-        } else if let Some(reps) = reps {
-            let history = &mut state.write().unwrap().user.history;
-            history.append_reps(&name, reps, weight.map(|w| w.value()));
-        } else {
-            panic!("expected duration or reps");
-        }
-    }
-
-    if in_workset(state, workout_name, exercise_name) {
-        append_result(state, workout_name, exercise_name, options);
-    }
-    advance_current(state, workout_name, exercise_name);
-}
-
-fn complete_set(
-    state: &mut SharedState,
-    workout_name: &str,
-    exercise_name: &str,
-    options: Option<VarRepsOptions>,
-) {
-    let exercise_name = ExerciseName(exercise_name.to_owned());
-    {
-        // Reset current set to start
-        let program = &mut state.write().unwrap().user.program;
-        let workout = program.find_mut(&workout_name).unwrap();
-        let exercise = workout.find_mut(&exercise_name).unwrap();
-        exercise.reset(None);
-    }
-
-    {
-        let history = &mut state.write().unwrap().user.history;
-        history.finish(&exercise_name);
-    }
-
-    if let Some(options) = options {
-        let mut new_expected = {
-            let history = &state.read().unwrap().user.history;
-            let program = &state.read().unwrap().user.program;
-            let workout = program.find(&workout_name).unwrap();
-            let exercise = workout.find(&exercise_name).unwrap();
-            get_var_reps_done(history, &exercise.name())
-        };
-
-        if options.advance == 1 {
-            // Advance weight (for VariableReps)
-            let new_weight = {
-                let weights = &state.read().unwrap().user.weights;
-                let program = &state.read().unwrap().user.program;
-                let workout = program.find(&workout_name).unwrap();
-                let exercise = workout.find(&exercise_name).unwrap();
-                exercise.advance_weight(weights).map(|w| w.value())
-            };
-
-            let program = &mut state.write().unwrap().user.program;
-            let workout = program.find_mut(&workout_name).unwrap();
-            let exercise = workout.find_mut(&exercise_name).unwrap();
-            exercise.set_weight(new_weight);
-            new_expected = match exercise {
-                Exercise::VariableReps(_, e) => e.min_expected().clone(),
-                Exercise::VariableSets(_, _) => new_expected, // not sure what something better would be
-                _ => panic!("expected Exercise::VariableReps"),
-            }
-        }
-        if options.update == 1 {
-            // Update expected (for VariableReps)
-            let program = &mut state.write().unwrap().user.program;
-            let workout = program.find_mut(&workout_name).unwrap();
-            let exercise = workout.find_mut(&exercise_name).unwrap();
-            match exercise {
-                Exercise::VariableReps(_, e) => {
-                    e.set_expected(new_expected);
-                }
-                Exercise::VariableSets(_, e) => {
-                    e.set_previous(new_expected);
-                }
-                _ => panic!("expected Exercise::VariableReps or VariableSets"),
-            }
-        }
     }
 }
 
@@ -1008,7 +741,7 @@ fn num_to_str(sets: &Vec<(i32, Option<f32>)>, unit: &str) -> String {
     }
 }
 
-fn get_var_reps_done(history: &History, name: &ExerciseName) -> Vec<i32> {
+pub fn get_var_reps_done(history: &History, name: &ExerciseName) -> Vec<i32> {
     let last = history.records(name).last().map_or(&None, |r| &r.sets);
     match last {
         Some(CompletedSets::Reps(v)) => v.iter().map(|t| t.0).collect(),
