45330801859e82e6ef5bdc8797ca54ffcd9d2078
Jesse Vorisek <jesse.vorisek@gmail.com>
2024-04-24 18:43:19 -0700

    Redid edit fixed reps

--- src/pages/exercise_post.rs
+++ src/pages/exercise_post.rs
@@ -225,41 +225,6 @@ pub fn post_revert_note(
     Ok(uri)
 }
 
-pub fn post_set_fixed_reps(
-    state: SharedState,
-    workout_name: &str,
-    exercise_name: &str,
-    warmups: Vec<FixedReps>,
-    worksets: Vec<FixedReps>,
-) -> Result<Uri, anyhow::Error> {
-    let exercise_name = ExerciseName(exercise_name.to_owned());
-
-    {
-        let program = &mut state.write().unwrap().user.program;
-        let workout = program.find_mut(&workout_name).unwrap();
-        let exercise = workout.find_mut(&exercise_name).unwrap();
-        let (d, e) = exercise.expect_fixed_reps_mut();
-        e.try_set_warmups(warmups)?;
-        e.try_set_worksets(worksets)?;
-
-        if !d.finished {
-            exercise.reset(exercise.started());
-        }
-    }
-
-    {
-        let user = &mut state.write().unwrap().user;
-        if let Err(e) = crate::persist::save(user) {
-            user.errors.push(format!("{e}")); // not fatal so we don't return an error
-        }
-    }
-
-    let path = format!("/exercise/{workout_name}/{exercise_name}");
-    let uri = url_escape::encode_path(&path);
-    let uri = uri.parse()?;
-    Ok(uri)
-}
-
 pub fn post_set_var_reps(
     state: SharedState,
     workout_name: &str,
