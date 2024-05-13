06474b5cb4e77190adfc7bcb8a955ea96db6c511
Jesse Vorisek <jesse.vorisek@gmail.com>
2024-05-13 09:18:04 -0700

    Added the new module files

---
+++ src/pages/views.rs
@@ -0,0 +1,11 @@
+mod exercise_page;
+mod exercise_post;
+mod overview_page;
+mod program_page;
+mod workout_page;
+
+pub use exercise_page::*;
+pub use exercise_post::*;
+pub use overview_page::*;
+pub use program_page::*;
+pub use workout_page::*;
